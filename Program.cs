75571036a4cece8a1686510a55440e668269d98e
SEBAANA YUSUF SALIM <114734084+salehjoseph@users.noreply.github.com>
2024-05-30 14:26:07 +0300

    Add files via upload
    Add looping logic to your code using the do-while and while statements in C#
    Role playing game battle challenge

--- Program.cs
+++ Program.cs
@@ -1,43 +1,54 @@
-﻿for (int i = 0; i <10; i++)
+﻿Random random = new Random();
+int current = random.Next(1, 11);
+
+do
 {
-    //Console.WriteLine(i);
-    //this breaks the iteration prematurely 
-    if(i==7) break;
+    current = random.Next(1, 11);
+
+    if (current >= 8)
+        continue;
+
+    Console.WriteLine(current);
+} while (current != 7);
+
+/*
+Monster was damaged and lost 1 health and now has 9 health.
+Hero was damaged and lost 1 health and now has 9 health.
+Monster was damaged and lost 7 health and now has 2 health.
+Hero was damaged and lost 6 health and now has 3 health.
+Monster was damaged and lost 9 health and now has -7 health.
+Hero wins!
+
+while (current >= 3)
+{
+    Console.WriteLine(current);
+    current = random.Next(1, 11);
 }
+Console.WriteLine($"Last number: {current}");
+*/
 
-//Looping through each element of an array using the for loop
-string[] names = { "Alex", "Eddie", "David", "Michael" };
-
-for (int i = 0; i < names.Length; i++)
+int Herohealth = 10;
+int Monsterhealth = 10;
+do
 {
-    if (names[i] == "David")
-    {
-        names[i] = "Sammy";
-    }
+    Random dice = new Random();
+
+    int roll1 = dice.Next(1, 11); // Heroes attack
+    Monsterhealth -= roll1;
+    Console.WriteLine(
+        $"Monster was damaged and lost {roll1} health and now has {Monsterhealth} health."
+    );
+    if(Monsterhealth <= 0) continue;
+   //Monsters attack
+    Herohealth -= roll1;
+    Console.WriteLine($"Hero was damaged and lost {roll1} health and now has {Herohealth} health.");
+} while (((Herohealth > 0) && (Monsterhealth > 0)));
+
+if (Herohealth > Monsterhealth)
+{
+    Console.WriteLine("Hero wins");
 }
-
-foreach (var name in names)
+else
 {
-  //  Console.WriteLine(name);
-}
-
-//Next we shall have the Fizz Bazz Challenge 
-
-// Here are the FizzBuzz rules that you need to implement in your code project:
-
-// Output values from 1 to 100, one number per line, inside the code block of an iteration statement.
-// When the current value is divisible by 3, print the term Fizz next to the number.
-// When the current value is divisible by 5, print the term Buzz next to the number.
-// When the current value is divisible by both 3 and 5, print the term FizzBuzz next to the number.
-
-for (int i = 1; i <= 100; i++)
-{
-    if ((i % 3 == 0) && (i % 5 == 0))
-        Console.WriteLine($"{i} - FizzBuzz");
-    else if (i % 3 == 0)
-        Console.WriteLine($"{i} - Fizz");
-    else if (i % 5 == 0)
-        Console.WriteLine($"{i} - Buzz");
-    else
-        Console.WriteLine($"{i}");
+    Console.WriteLine("Monster Wins");
 }
