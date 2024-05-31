fb5695cd920e8d7b480cf72390592b1dc1f56031
salehjoseph <www.sebyu@gmail.com>
2024-05-31 12:37:52 +0300

    Validate Integer Input in C#

--- CodeProjectOne/Program.cs
+++ CodeProjectOne/Program.cs
@@ -1,4 +1,4 @@
-/*string? readResult;
+string? readResult;
 bool validEntry = false;
 Console.WriteLine("Enter a string containing at least three characters:");
 do
@@ -15,7 +15,7 @@ do
             Console.WriteLine("Your input is invalid, please try again.");
         }
     }
-} while (validEntry == false);*/
+} while (validEntry == false);
 
   int userInput;
             bool isValidNumber = false;
