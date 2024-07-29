bd1d8c40fa5a43537ba5e7b0166b9f03fd8c648a
SEBAANA YUSUF SALIM <114734084+salehjoseph@users.noreply.github.com>
2024-07-29 06:58:38 +0300

    Understanding return type Syntax in C# methods

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -1,35 +1,29 @@
-string[,] corporate = 
-{
-    {"Robert", "Bavin"}, {"Simon", "Bright"},
-    {"Kim", "Sinclair"}, {"Aashrita", "Kamath"},
-    {"Sarah", "Delucchi"}, {"Sinan", "Ali"}
-};
+double total = 0;
+double minimumSpend = 30.00;
 
-string[,] external = 
-{
-    {"Vinnie", "Ashton"}, {"Cody", "Dysart"},
-    {"Shay", "Lawrence"}, {"Daren", "Valdes"}
-};
+double[] items = {15.97, 3.50, 12.25, 22.99, 10.98};
+double[] discounts = {0.30, 0.00, 0.10, 0.20, 0.50};
 
-string externalDomain = "hayworth.com";
-
-for (int i = 0; i < corporate.GetLength(0); i++) 
+for (int i = 0; i < items.Length; i++)
 {
-    string corp = "@contoso.com";
-    string firstName =  corporate[i,0];
-    string lastName = corporate[i,1];
-    // display internal email addresses
-   
-    string email = firstName.Substring(0,2) + lastName +corp;
-    Console.WriteLine(email.ToLower());
+    total += GetDiscountedPrice(i);
 }
 
-for (int i = 0; i < external.GetLength(0); i++) 
-{
-    // display external email addresses
-    string firstName = external[i,0];
-    string lastName = external[i,1];
+total -= TotalMeetsMinimum() ? 5.00 : 0.00;
 
-    string email = firstName.Substring(0,2) + lastName + externalDomain;
-    Console.WriteLine(email.ToLower());
+Console.WriteLine($"Total: ${FormatDecimal(total)}");
+
+double GetDiscountedPrice(int itemIndex)
+{
+    return items[itemIndex] * (1 - discounts[itemIndex]);
+}
+
+bool TotalMeetsMinimum()
+{
+    return total >= minimumSpend;
+}
+
+string FormatDecimal(double input)
+{
+    return input.ToString().Substring(0, 5);
 }
